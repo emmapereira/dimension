ed70eb614699eaaab6787e6e38d2270bf7b4cc72
emmapereira <emmapereira23@gmail.com>
2023-01-29 17:08:37 +0100

    changes on contact page

--- src/components/FormCell.js
+++ src/components/FormCell.js
@@ -1,11 +1,7 @@
 import React from 'react';
 
-export default (props) => {
-   
- return (
-    <article id="contact" key="contact">
-        <h2 className="major">Contact</h2>
-        <form method="post" action={props.action}>
+/*
+<form method="post" action={props.action}>
             <div className="field half first">
                 <label for="name">Name</label>
                 <input type="text" name="name" id="name" />
@@ -23,11 +19,33 @@ export default (props) => {
                 <li><input type="reset" value="Reset" /></li>
             </ul>
         </form>
+
         <ul className="icons">
             <li><a href="#" className="icon fa-twitter"><span className="label">Twitter</span></a></li>
             <li><a href="#" className="icon fa-facebook"><span className="label">Facebook</span></a></li>
             <li><a href="#" className="icon fa-instagram"><span className="label">Instagram</span></a></li>
             <li><a href="#" className="icon fa-github"><span className="label">GitHub</span></a></li>
+        </ul>
+*/ 
+
+export default (props) => {
+   
+ return (
+    <article id="contact" key="contact">
+        <h2 className="major">Contact</h2>
+        
+        
+        <ul className="icons">
+            <li><a href="https://www.linkedin.com/in/emmapereirab" className="icon fa-linkedin"><span className="label">Twitter</span></a></li>
+            <li>Linkedin: emmapereirab</li>
+            <br></br>
+            <br></br>
+            <li><a href="https://github.com/emmapereira" className="icon fa-github"><span className="label">GitHub</span></a></li>
+            <li>Github: emmapereira</li>
+            <br></br>
+            <br></br>
+            <li><a className="icon fa-google"><span className="label">GitHub</span></a></li>
+            <li>Email: emmapereira23@gmail.com</li>
         </ul>
     </article>
 
